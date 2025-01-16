8b8f33d6c50ee2cdd7c50d66ec287f8a6b345303
Christian Treffs <ctreffs@gmail.com>
2025-01-16 11:18:31 +0100

    Format JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,12 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    const targetSection = document.querySelector('#hero');
-    const homeBottom = targetSection.offsetTop + targetSection.offsetHeight; // Bottom position of #home section
-  
-    if (window.scrollY > homeBottom) {
-      navbar.classList.add('navbar-solid');
-    } else {
-      navbar.classList.remove('navbar-solid');
-    }
-  });
+// Change navbar background color on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const targetSection = document.querySelector("#hero");
+  const homeBottom = targetSection.offsetTop + targetSection.offsetHeight; // Bottom position of #home section
+
+  if (window.scrollY > homeBottom) {
+    navbar.classList.add("navbar-solid");
+  } else {
+    navbar.classList.remove("navbar-solid");
+  }
+});
